b4f8d4169c7e77aaad1ae7b793c24822e1b25adb
Artur Felipe <artursantiago505@gmail.com>
2022-08-28 19:06:41 -0300

    shifts

---
+++ Exercise 2-10.py
@@ -0,0 +1,9 @@
+shift = int(input('In which shift do you study? '))
+if shift == 1:
+    print('good morning')
+elif shift == 2:
+    print('good morning')
+elif shift == 3:
+    print('good night')
+else:
+    print('invalid shift')
