93e72e8033a4db7d5c4daa6c61fe62def59692a1
Artur Felipe <artursantiago505@gmail.com>
2022-08-29 11:34:32 -0300

    withdrawal

--- Exercise 2-21.py
+++ Exercise 2-21.py
@@ -4,24 +4,27 @@ tens = ((value - units) % 100)
 hundreds = (value - units - tens) % 1000
 fives = 0
 ones = 0
-tensd = 0
+tensdb = 0
 fifties = 0
 hundreddb = 0
-if units < 5:
-    ones = units - tens
-if 5 <= units < 10:
-    fives = fives + 1
-    ones = units -5
-if 10 <= tens < 50:
-    tensdb = tens / 10
-if 50 <= tens <100:
-    fifties = fifties + 1
-    tensdb = (tens -50)/10
-if hundreds>100:
-    hundreddb = hundreddb + (hundreds / 100)
+if value<10 or value>600:
+    print('the value is not valid')
+else:
+    if units < 5:
+        ones = units
+    if 5 <= units < 10:
+        fives = fives + 1
+        ones = units -5
+    if 10 <= tens < 50:
+        tensdb = tens / 10
+    if 50 <= tens <100:
+        fifties = fifties + 1
+        tensdb = (tens -50)/10
+    if hundreds >= 100:
+        hundreddb = hundreddb + (hundreds / 100)
 
-print('ones: ', ones)
-print('fives: ', fives)
-print('tens: ', int(tensdb))
-print('fifties: ', fifties)
-print('hudreds: ', int(hundreddb))
+    print('ones: ', ones)
+    print('fives: ', fives)
+    print('tens: ', int(tensdb))
+    print('fifties: ', fifties)
+    print('hundreds: ', int(hundreddb))
