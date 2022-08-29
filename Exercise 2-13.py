17ab010fd94391fa57df727f89da2912aa361342
Artur Felipe <artursantiago505@gmail.com>
2022-08-28 22:09:14 -0300

    week days

---
+++ Exercise 2-13.py
@@ -0,0 +1,17 @@
+num = int(input('Type a number relative to a week day: '))
+if num == 1:
+    print('Sunday')
+elif num == 2:
+    print('Monday')
+elif num == 3:
+    print('Tuesday')
+elif num == 4:
+    print('Wednesday')
+elif num == 5:
+    print('Thursday')
+elif num == 6:
+    print('Friday')
+elif num == 7:
+    print('Saturday')
+else:
+    print('Invalid number')
